f799037955fc8944ef59d36e83a0b73c899916dc
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2025-04-15 18:11:48 +0200

    feat: initialize front-end project

---
+++ Frontend/lexica-next-front/README.md
@@ -0,0 +1,1 @@
+# lexica-next-front
---
+++ Frontend/lexica-next-front/vite.config.ts
@@ -0,0 +1,28 @@
+import * as process from 'process';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  const outputDir = process.env.VITE_OUTPUT_DIR || '../../LexicaNext.WebApp/wwwroot';
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: outputDir,
+      sourcemap: true,
+    },
+    server: isProduction
+      ? undefined
+      : {
+          proxy: {
+            '/api': {
+              target: 'https://localhost:7226',
+              changeOrigin: true,
+              secure: false,
+            },
+          },
+        },
+  };
+});
