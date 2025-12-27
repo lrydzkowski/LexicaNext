cd153dcc508c770c17ad16f680a2640dcdb2e016
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2025-12-27 10:36:45 +0100

    remove unnecessary validation error code

--- LexicaNext.Core/Common/Infrastructure/Models/ValidationErrorCodes.cs
+++ LexicaNext.Core/Common/Infrastructure/Models/ValidationErrorCodes.cs
@@ -3,12 +3,7 @@ namespace LexicaNext.Core.Common.Infrastructure.Models;
 internal static class ValidationErrorCodes
 {
     public const string UniquenessValidator = "UniquenessValidator";
-
     public const string ExistenceValidator = "ExistenceValidator";
-
     public const string ValueInSetValidator = "ValueInSetValidator";
-
     public const string FormatValidator = "FormatValidator";
-
-    public const string RangeValidator = "RangeValidator";
 }
