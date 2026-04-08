b0d648c2a993abeac9c4f005a01ba87be08dc861
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2026-04-08 19:04:26 +0200

    ignore polish diacritics

--- Frontend/lexica-next-front/src/utils/utils.ts
+++ Frontend/lexica-next-front/src/utils/utils.ts
@@ -1,11 +1,25 @@
 export const serialize = (translations: string[] | undefined): string => translations?.join(', ') || '';
 
+const removePolishDiacritics = (text: string): string =>
+  text
+    .replace(/ą/g, 'a')
+    .replace(/ć/g, 'c')
+    .replace(/ę/g, 'e')
+    .replace(/ł/g, 'l')
+    .replace(/ń/g, 'n')
+    .replace(/ó/g, 'o')
+    .replace(/ś/g, 's')
+    .replace(/ź/g, 'z')
+    .replace(/ż/g, 'z');
+
 export const compareAnswers = (userAnswer: string, correctAnswers: string[]): boolean => {
   const userAnswers = userAnswer
     .split(',')
-    .map((x) => x.trim().toLowerCase())
+    .map((x) => removePolishDiacritics(x.trim().toLowerCase()))
     .sort();
-  const processedCorrectAnswers = correctAnswers.map((correctAnswer) => correctAnswer.trim().toLowerCase()).sort();
+  const processedCorrectAnswers = correctAnswers
+    .map((correctAnswer) => removePolishDiacritics(correctAnswer.trim().toLowerCase()))
+    .sort();
   const correct = userAnswers.join(',') === processedCorrectAnswers.join(',');
 
   return correct;
