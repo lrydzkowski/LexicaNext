0d3ac7e99d37eebea970bd03b11f85152c89f8fd
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2026-09-20 16:26:54 +0200

    change local configuration

--- Frontend/lexica-next-front/certificates/generate_cert.ps1
+++ Frontend/lexica-next-front/certificates/generate_cert.ps1
@@ -1,4 +1,4 @@
-$hostIp = '192.168.50.30'
+$hostIp = '127.0.0.1'
 $cert = New-SelfSignedCertificate `
  -Subject "CN=$hostIp" `
  -Type SSLServerAuthentication `
@@ -7,8 +7,8 @@ $cert = New-SelfSignedCertificate `
  -KeyExportPolicy Exportable
 
 $certPassword = Read-Host 'Certificate password' -AsSecureString
-Export-PfxCertificate -Cert $cert -FilePath ".\certificates\lan.pfx" -Password $certPassword
-Export-Certificate -Cert $cert -FilePath ".\certificates\lan.cer"
+Export-PfxCertificate -Cert $cert -FilePath ".\lan.pfx" -Password $certPassword
+Export-Certificate -Cert $cert -FilePath ".\lan.cer"
 
 $env:LEXICA_HTTPS_PFX = ".\certificates\lan.pfx"
 $env:LEXICA_HTTPS_PASSWORD = [System.Net.NetworkCredential]::new('', $certPassword).Password
